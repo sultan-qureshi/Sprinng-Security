6f0587b6f2dd73c463a1c68bf691e581549eae13
sulqures <sulqures@LIN51017324.corp.capgemini.com>
2021-11-19 21:35:10 +0530

    Spring Security Questions

---
+++ SpringSecurity/springsecurity/src/main/java/springsecurity/welcome.java
@@ -0,0 +1,12 @@
+package springsecurity;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//@RestController
+public class welcome {
+	//@GetMapping("/")
+	public String demomsg() {
+		return "....Welcome....";
+	}
+
+}
